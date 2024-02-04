9c60c0ca46943bfac0434e00f93c803472c5a757
Jovid <jovid.9@gmail.com>
2024-02-04 20:12:56 +0500

    cleaning up the seed file

--- api/user_handler_test.go
+++ api/user_handler_test.go
@@ -2,65 +2,35 @@ package api
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 
-	"log"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/JovidYnwa/hostel-reservation/db"
 	"github.com/JovidYnwa/hostel-reservation/types"
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-const (
-	testdburi = "mongodb://localhost:27017"
-	dbname = "hostel-reservation-test"
-)
-
-type testdb struct {
-	db.UserStore
-}
-
-func (tdb *testdb) teardown(t *testing.T) {
-	if err := tdb.UserStore.Drop(context.TODO()); err !=nil {
-		t.Fatal(err)
-	}
-}
-
-func setup(t *testing.T) *testdb {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(testdburi))
-	if err != nil {
-		log.Fatal(err)
-	}
-	return &testdb{
-		UserStore: db.NewMongoUserStore(client),
-	}
-}
 
 func TestPostUser(t *testing.T) {
-	tdb :=setup(t)
+	tdb := setup(t)
 	defer tdb.teardown(t)
 
 	app := fiber.New()
 	userHandler := NewUserHandler(tdb.UserStore)
 	app.Post("/", userHandler.HandlePostUser)
 
-	params :=types.CreateUserParams{
-		Email: "jalsdf9@asdf.com",
+	params := types.CreateUserParams{
+		Email:     "jalsdf9@asdf.com",
 		FirstName: "Jojo",
-		LastName: "Nono",
-		Password: "some_pass",
+		LastName:  "Nono",
+		Password:  "some_pass",
 	}
 
 	b, _ := json.Marshal(params)
 	req := httptest.NewRequest("POST", "/", bytes.NewReader(b))
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := app.Test(req)
-	if err != nil{
+	if err != nil {
 		t.Error(err)
 	}
 	var user types.User
--- scripts/seed.go
+++ scripts/seed.go
@@ -9,82 +9,18 @@ import (
 	"github.com/JovidYnwa/hostel-reservation/api"
 	"github.com/JovidYnwa/hostel-reservation/db"
 	"github.com/JovidYnwa/hostel-reservation/db/fixtures"
-	"github.com/JovidYnwa/hostel-reservation/types"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
-	client *mongo.Client
-	roomStore db.RoomStore
-	hostelSotre db.HostelStore
-	userStore db.UserStore
+	client       *mongo.Client
+	roomStore    db.RoomStore
+	hostelSotre  db.HostelStore
+	userStore    db.UserStore
 	bookingStore db.BookingStore
-	ctx = context.Background()
+	ctx          = context.Background()
 )
-
-func seedUser(isAdmin bool, fname string, lname string, email string, password string) *types.User {
-	user, err := types.NewUserFromParams(types.CreateUserParams{
-		Email: email,
-		FirstName: fname,
-		LastName: lname,
-		Password: password,
-	})
-	if err != nil{
-		log.Fatal(err)
-	}
-	user.IsAdmin = isAdmin
-	inserteUser, err := userStore.InsertUser(context.TODO(), user)
-	if err != nil{
-		log.Fatal(err)
-	}
-	fmt.Printf("%s -> %s\n", user.Email, api.CreateTokenFromUser(user))
-	return inserteUser
-}
-
-func seedBooking(userID, roomID primitive.ObjectID, from, till time.Time) {
-	booking := &types.Booking{
-		UserID: userID,
-		RoomID: roomID,
-		FromDate: from,
-		TillDate: till,
-	}
-	resp, err := bookingStore.InsertBooking(context.Background(), booking); 
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("booking:", resp.ID)
-}
-
-func seedHostel(name string, location string, rating int) *types.Hostel {
-	hostel := types.Hostel{
-		Name:     name,
-		Location: location,
-		Rooms: []primitive.ObjectID{},
-		Rating: rating,
-	}
-
-	insertedHostel, err := hostelSotre.InsertHostel(ctx, &hostel)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return insertedHostel
-}
-
-func seedRoom(size string, ss bool, price float64, hostelID primitive.ObjectID) *types.Room {
-	room := &types.Room{
-		Size: size,
-		Seaside: ss,
-		Price: price,
-		HostelID: hostelID,
-	}
-	insertedRoom, err := roomStore.InsertRoom(context.Background(), room)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return insertedRoom
-}
 
 func main() {
 	var err error
@@ -92,37 +28,24 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err :=client.Database(db.DBNAME).Drop(ctx); err != nil{
+	if err := client.Database(db.DBNAME).Drop(ctx); err != nil {
 		log.Fatal(err)
 	}
 	hostelSotre := db.NewMongoHostelStore(client)
-	store := &db.Store {
-		User: db.NewMongoUserStore(client),
+	store := &db.Store{
+		User:    db.NewMongoUserStore(client),
 		Booking: db.NewMongoBookingStore(client),
-		Room: db.NewMongoRoomStore(client, hostelSotre),
-		Hostel: hostelSotre,
+		Room:    db.NewMongoRoomStore(client, hostelSotre),
+		Hostel:  hostelSotre,
 	}
 	user := fixtures.AddUser(store, "test", "testi", false)
+	fmt.Println("test ->", api.CreateTokenFromUser(user))
 	admin := fixtures.AddUser(store, "admin", "testi", true)
-	fmt.Println("admin ->", admin)
+	fmt.Println("admin ->", api.CreateTokenFromUser(admin))
 	hostel := fixtures.AddHostel(store, "some hostel", "casablanka", 5, nil)
 	room := fixtures.AddRoom(store, "large", true, 88.44, hostel.ID)
 	booking := fixtures.AddBooking(*store, user.ID, room.ID, time.Now(), time.Now().AddDate(0, 0, 5))
-	fmt.Println(booking)	
+	fmt.Println(booking)
 }
 
-func init(){
-	var err error
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err :=client.Database(db.DBNAME).Drop(ctx); err != nil{
-		log.Fatal(err)
-	}
-	hostelSotre = db.NewMongoHostelStore(client)
-	roomStore = db.NewMongoRoomStore(client, hostelSotre)
-	userStore = db.NewMongoUserStore(client)
-	bookingStore = db.NewMongoBookingStore(client)
 
-}
